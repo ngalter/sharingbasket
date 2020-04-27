93a9cb274e4054613ad37c418fdd5c1fec8404ac
Nancy Alter <nancy.alter@gmail.com>
2020-04-27 09:22:32 -0400

    again

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-
 // Requiring our routes
 require("./routes/api-routes.js")(app);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 // We need to use sessions to keep track of our user's login status
 app.use(
