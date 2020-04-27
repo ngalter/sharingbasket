bcf80f2ea09bc7fb07e73a17bd9a472ba668aa0d
Nancy Alter <nancy.alter@gmail.com>
2020-04-27 09:01:58 -0400

    routing changes

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
     <Router>
     <div>
       <Switch>
-        <Route exact path={["/", "/home"]}>
+        <Route exact path={"/"}>
           <HomePage />
         </Route>
         <Route exact path={["/login-client"]}>
