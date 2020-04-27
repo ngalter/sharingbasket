0ab44c5ecad045ac6b9f604316223e9d9ee0dc92
Nancy Alter <nancy.alter@gmail.com>
2020-04-27 08:11:21 -0400

    config

--- server.js
+++ server.js
@@ -17,11 +17,21 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Requiring our routes
-
 require("./routes/api-routes.js")(app);
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 // We need to use sessions to keep track of our user's login status
 app.use(
